d07cbb39877b7f1534433fc859f1fbb2348d0c11
Eleazar Alfredo, ALFRIADEZ YRIARTE <ealfriadez@gmail.com>
2024-07-05 21:27:16 -0500

    first commit

---
+++ src/main/java/pe/edu/unfv/mypersonablog/MyPersonaBlogApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.unfv.mypersonablog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPersonaBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPersonaBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/unfv/mypersonablog/MyPersonaBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.unfv.mypersonablog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPersonaBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
